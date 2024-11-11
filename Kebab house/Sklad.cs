244467d18352034b787d9f8ba997eddd42fba929
Adam Bureš <buresa23@spstrutnov.cz>
2024-11-11 13:19:49 +0100

    Založení skladu

---
+++ Kebab house/Sklad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kebab_house
+{
+    internal class Sklad
+    {
+        public int ZasobaMasa = 200;
+        //private int ZasobaSalat = 100;
+        //private int ZasobaZelí = 50;
+
+
+        //public int StavMasa(ZasobaMasa - SpotrebaMasa);
+
+
+    }
+}
