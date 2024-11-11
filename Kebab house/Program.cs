d9bcbc788c18b0b4463f9595a6b9936c35129090
Adam Bureš <buresa23@spstrutnov.cz>
2024-11-11 12:38:03 +0100

    Vytvoření projektu

---
+++ Kebab house/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kebab_house
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
